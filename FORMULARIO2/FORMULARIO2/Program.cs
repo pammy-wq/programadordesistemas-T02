4068c7bdff828cf9d129f147bd9ddd177ce5fa4a
everett-wq <paoli.lemos@gmail.com>
2025-12-10 18:40:48 -0300

    primeiro commit

---
+++ FORMULARIO2/FORMULARIO2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+
+    {
+        float a = 0;
+        float b = 0;
+        float c = 0;
+        bool continuarLoop = true;
+
+        while (continuarLoop == true)
+        {
+            b++;
+
+            Console.WriteLine("insira um valor:");
+            a += int.Parse(Console.ReadLine());
+
+            Console.WriteLine(" digite a para continuar ou b para parar:");
+            string opcaoEscolhida = Console.ReadLine();
+            if (opcaoEscolhida == "b")
+            {  continuarLoop=false; }
+        }
+
+        c = a / b;
+        Console.WriteLine(c);
+    }
+}
---
+++ treinandoparaprova/treinandoparaprova/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("\n-----Ativide1---");
+
+int idade;
+Console.WriteLine("qual a sua idade?"); 
+idade = int.Parse(Console.ReadLine()); // o int.parse pega uma string e tranforma para int
+
+
+// aqui eu criei uma variabel int idade, ela guarda um numero inteiro
+//depois eu pergunto a idade do usuario com console.writeline.
+// o usuario digita um texto, console.readline, sempre le como string, mesmo que o usuario digite 18
+// int.parse converte essa string para o numero 
+
+/* int.Parse(Console.ReadLine());*/ //isso transforma, por exemplo "18" -> 18 (numero)
+                                    // a variavel idade recebe esse numero int.parse( console. readline));
+
+/*Console.WriteLine() ➜ mostra a pergunta
+
+Console.ReadLine() ➜ pega a resposta (como texto)
+
+int.Parse() ➜ transforma o texto em número
+
+idade = ... ➜ guarda o número dentro da variável idade*/
+
+Console.WriteLine("\nAtividade2");
+
+int numero1;
+int numero2;
+Console.WriteLine("qual seria o numero?");
+numero1 = int.Parse(Console.ReadLine());
+Console.WriteLine("proximo numero");
+numero2 = int.Parse(Console.ReadLine());
+int soma = numero1 + numero2;
+Console.WriteLine("A soma é: " + soma);
+
+
