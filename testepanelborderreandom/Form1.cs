c2cc6f7ad847774d6060fe06ae2df77dbfa1c574
pammy-wq <solars470@gmail.com>
2026-03-23 15:24:10 -0300

    subindo atualização

---
+++ testepanelborderreandom/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testepanelborderreandom
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ArredondarPainel(Panel panel, int raio)
+        {
+            GraphicsPath path = new GraphicsPath();
+
+            path.AddArc(0, 0, raio, raio, 180, 90);
+            path.AddArc(panel.Width - raio, 0, raio, raio, 270, 90);
+            path.AddArc(panel.Width - raio, panel.Height - raio, raio, raio, 0, 90);
+            path.AddArc(0, panel.Height - raio, raio, raio, 90, 90);
+
+            path.CloseFigure();
+
+            panel.Region = new Region(path);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ArredondarPainel(panel1, 30);
+        }
+    }
+}
