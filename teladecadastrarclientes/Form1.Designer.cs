48ffff3471a25de34adc11b0231c2e26f8c2a178
pammy-wq <solars470@gmail.com>
2026-04-16 16:58:35 -0300

    crud

---
+++ teladecadastrarclientes/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace teladecadastrarclientes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCadastrarClientes = new System.Windows.Forms.Label();
+            this.lblNomeCompleto = new System.Windows.Forms.Label();
+            this.lblID = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblCadastrarClientes
+            // 
+            this.lblCadastrarClientes.AutoSize = true;
+            this.lblCadastrarClientes.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCadastrarClientes.Location = new System.Drawing.Point(123, 57);
+            this.lblCadastrarClientes.Name = "lblCadastrarClientes";
+            this.lblCadastrarClientes.Size = new System.Drawing.Size(289, 45);
+            this.lblCadastrarClientes.TabIndex = 0;
+            this.lblCadastrarClientes.Text = "Cadastrar Clientes";
+            // 
+            // lblNomeCompleto
+            // 
+            this.lblNomeCompleto.AutoSize = true;
+            this.lblNomeCompleto.Font = new System.Drawing.Font("Segoe UI Semibold", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNomeCompleto.Location = new System.Drawing.Point(59, 133);
+            this.lblNomeCompleto.Name = "lblNomeCompleto";
+            this.lblNomeCompleto.Size = new System.Drawing.Size(92, 13);
+            this.lblNomeCompleto.TabIndex = 1;
+            this.lblNomeCompleto.Text = "Nome Completo";
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(75, 253);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(18, 13);
+            this.lblID.TabIndex = 2;
+            this.lblID.Text = "ID";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(62, 149);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(295, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 582);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.lblNomeCompleto);
+            this.Controls.Add(this.lblCadastrarClientes);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCadastrarClientes;
+        private System.Windows.Forms.Label lblNomeCompleto;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
